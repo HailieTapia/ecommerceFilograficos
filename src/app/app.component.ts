b6e0abf92672820d5d5332fc9ab623061c7bb6ed
HailieTapia <136946909+HailieTapia@users.noreply.github.com>
2025-02-09 09:58:15 -0600

    modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	new file:   src/app/components/header/header.component.css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, CommonModule],  // Aquí solo necesitas importar RouterModule y CommonModule
+  imports: [HeaderComponent,RouterOutlet, RouterModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
