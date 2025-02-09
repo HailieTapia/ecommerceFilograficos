b02da1163d508634374ca8cffbf4573cbe8a991b
HailieTapia <136946909+HailieTapia@users.noreply.github.com>
2025-02-09 10:01:15 -0600

    new file:   src/app/components/header/header.component.html 	new file:   src/app/components/header/header.component.ts

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component,OnInit  } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent{
+
+  constructor(private userService: UserService, private router: Router) {}
+
+
+}
