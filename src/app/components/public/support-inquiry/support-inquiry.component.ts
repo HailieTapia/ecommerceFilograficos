ba29f445fe1000b21bc4163b732b7422822a295c
LUISGERARDO11 <luis34julieta@gmail.com>
2025-02-18 10:55:14 -0600

    uso de reCAPTCHA en el formulario de consulta

--- src/app/components/public/support-inquiry/support-inquiry.component.ts
+++ src/app/components/public/support-inquiry/support-inquiry.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { SupportInquiryService } from '../../services/support-inquiry.service';
 import { CompanyService } from '../../services/company.service';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { environment } from '../../../environments/config';
 
 @Component({
   selector: 'app-support-inquiry',
@@ -12,11 +13,12 @@ import { HttpClientModule } from '@angular/common/http';
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule, HttpClientModule],
 })
-export class SupportInquiryComponent implements OnInit {
+export class SupportInquiryComponent implements OnInit, AfterViewInit {
   supportForm: FormGroup;
   companyInfo: any;
   submitted = false;
   errorMessage: string = '';
+  siteKey = environment.recaptchaSiteKey;
 
   constructor(
     private fb: FormBuilder,
@@ -28,11 +30,32 @@ export class SupportInquiryComponent implements OnInit {
       user_email: ['', [Validators.required, Validators.email]],
       subject: ['', [Validators.required, Validators.minLength(5)]],
       message: ['', [Validators.required, Validators.maxLength(500)]],
+      recaptchaToken: ['', Validators.required]
     });
   }
 
   ngOnInit(): void {
     this.getCompanyInfo();
+  }
+
+  ngAfterViewInit() {
+    this.loadRecaptcha();
+  }
+
+  loadRecaptcha() {
+    if ((window as any).grecaptcha) {
+      (window as any).grecaptcha.render('support-recaptcha-container', {
+        sitekey: this.siteKey,
+        callback: (response: string) => this.onCaptchaResolved(response),
+        'expired-callback': () => this.supportForm.patchValue({ recaptchaToken: '' }),
+      });
+    } else {
+      console.error('reCAPTCHA no está disponible');
+    }
+  }
+
+  onCaptchaResolved(token: string) {
+    this.supportForm.patchValue({ recaptchaToken: token });
   }
 
   getCompanyInfo(): void {
@@ -64,8 +87,7 @@ export class SupportInquiryComponent implements OnInit {
         }, 3000);
       },
       (error) => {
-        this.errorMessage = 'Hubo un error al enviar tu mensaje. Por favor, inténtalo de nuevo.';
-        // Ocultar mensaje de error después de 5 segundos
+        this.errorMessage = error.error?.message || 'Hubo un error al enviar tu mensaje. Por favor, inténtalo de nuevo.';
         setTimeout(() => {
           this.errorMessage = '';
         }, 5000);
