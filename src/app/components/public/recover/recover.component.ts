6e7f49aadab37be338858fb7366baf74049fca93
HailieTapia <136946909+HailieTapia@users.noreply.github.com>
2025-02-23 16:23:22 -0600

    modified:   src/app/components/public/recover/recover.component.html 	modified:   src/app/components/public/recover/recover.component.ts

--- src/app/components/public/recover/recover.component.ts
+++ src/app/components/public/recover/recover.component.ts
@@ -4,10 +4,13 @@ import { PasswordService } from '../../services/password.service';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { PasswordToggleComponent } from '../../administrator/shared/password-toggle/password-toggle.component';
+import { ToastComponent } from '../../administrator/shared/toast/toast.component';
+import { ToastService } from '../../services/toastService';
 @Component({
   selector: 'app-recover',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [ToastComponent, PasswordToggleComponent, CommonModule, ReactiveFormsModule],
   templateUrl: './recover.component.html',
   styleUrl: './recover.component.css'
 })
@@ -19,69 +22,71 @@ export class RecoverComponent {
   email: string = "";
   recoveryToken: string = "";
 
-  constructor(private router: Router,private fb: FormBuilder, private passwordService: PasswordService) {
-    // Formulario para iniciar la recuperación de contraseña
+  constructor(private toastService: ToastService, private router: Router, private fb: FormBuilder, private passwordService: PasswordService) {
     this.recoveryForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]]
     });
-
-    // Formulario para verificar OTP
     this.otpForm = this.fb.group({
-      otp: ['', [Validators.required]]
+      otp0: ['', [Validators.required]],
+      otp1: ['', [Validators.required]],
+      otp2: ['', [Validators.required]],
+      otp3: ['', [Validators.required]],
+      otp4: ['', [Validators.required]],
+      otp5: ['', [Validators.required]],
+      otp6: ['', [Validators.required]],
+      otp7: ['', [Validators.required]]
     });
-
-    // Formulario para restablecer la contraseña
     this.resetForm = this.fb.group({
       newPassword: ['', [Validators.required, Validators.minLength(8)]],
       confirmPassword: ['', [Validators.required, Validators.minLength(8)]]
     });
   }
-
-
   // Método para iniciar el proceso de recuperación de contraseña
   initiateRecovery() {
     if (this.recoveryForm.invalid) {
-      console.error('Por favor ingresa un correo válido');
+      this.toastService.showToast('Por favor ingresa un correo válido', 'error');
       return;
     }
     this.passwordService.initiatePasswordRecovery(this.recoveryForm.value).subscribe(
       (response) => {
-        console.log('Se ha enviado un código de recuperación a tu correo');
+        const successMessage = response?.message || 'Se ha enviado un código de recuperación a tu correo';
+        this.toastService.showToast(successMessage, 'success');
+
         this.email = this.recoveryForm.value.email;
-        this.stage = 2; // Pasar a la siguiente etapa (verificación OTP)
+        this.stage = 2; 
       },
       (error) => {
         const errorMessage = error?.error?.message || 'Error al iniciar el proceso de recuperación';
-        console.error('MENSAJE DE MI BACK.', errorMessage);
+        this.toastService.showToast(errorMessage, 'error');
       }
     );
   }
 
-
-  //metodo para verificar el codigo otp para recuperacion de contraseña
-  verifyOTP() {
-    if (this.otpForm.invalid) {
-      console.error('Por favor ingresa un código OTP válido');
-      return;
+  moveFocus(index: number, event: any): void {
+    const nextInput = event.target.nextElementSibling;
+    if (nextInput && event.target.value) {
+      nextInput.focus();
     }
+  }
+  verifyOTP() {
+    const otp = Object.values(this.otpForm.value).join('');
 
     const requestData = {
       email: this.email,
-      otp: this.otpForm.value.otp
+      otp: otp
     };
-
-    console.log('Enviando datos para verificar OTP:', requestData);
 
     this.passwordService.verifyOTP(requestData).subscribe(
       (response) => {
-        console.log('Respuesta del backend:', response);
-        console.log('OTP verificado correctamente');
-        this.recoveryToken = this.otpForm.value.otp;
-        this.stage = 3; // Pasar a la siguiente etapa (restablecer contraseña)
+        const successMessage = response?.message || 'Verificación correcta.';
+        this.toastService.showToast(successMessage, 'success');
+
+        this.recoveryToken = otp;
+        this.stage = 3;
       },
       (error) => {
-        const errorMessage = error?.error?.message || 'El OTP ingresado es incorrecto o ha expirado.';
-        console.error('MENSAJE DE MI BACK.', errorMessage);
+        const errorMessage = error?.error?.message || 'El código ingresado es incorrecto o ha expirado.';
+        this.toastService.showToast(errorMessage, 'error');
       }
     );
   }
@@ -99,14 +104,17 @@ export class RecoverComponent {
 
     this.passwordService.resetPassword(credentials).subscribe(
       (response) => {
-        console.error('Contraseña restablecida exitosamente');
-        this.router.navigate(['login']); 
+        const successMessage = response?.message || 'Contraseña restablecida exitosamente';
+        this.toastService.showToast(successMessage, 'success');
+
+        this.router.navigate(['login']);
         this.recoveryForm.reset();
         this.otpForm.reset();
         this.resetForm.reset();
       },
       (error) => {
-        console.error('Error al restablecer la contraseña');
+        const errorMessage = error?.error?.message || 'Error al restablecer la contraseña.';
+        this.toastService.showToast(errorMessage, 'error');
       }
     );
   }
