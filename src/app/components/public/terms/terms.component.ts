f9ef2da25bf26409154e73d369a024c6f0013866
HailieTapia <136946909+HailieTapia@users.noreply.github.com>
2025-02-26 09:15:12 -0600

    modified:   src/app/components/public/disclaimer/disclaimer.component.html 	modified:   src/app/components/public/disclaimer/disclaimer.component.ts 	modified:   src/app/components/public/policy/policy.component.ts 	modified:   src/app/components/public/terms/terms.component.ts

--- src/app/components/public/terms/terms.component.ts
+++ src/app/components/public/terms/terms.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RegulatoryService } from '../../services/regulatory.service';
 
 @Component({
   selector: 'app-terms',
@@ -8,5 +9,29 @@ import { Component } from '@angular/core';
   styleUrl: './terms.component.css'
 })
 export class TermsComponent {
-
+  terminosCondiciones: any;
+  titulo: string = '';
+  versionDocumento: string = '';
+  fechaVigencia: string = '';
+  constructor(private regulatoriosService: RegulatoryService) { }
+  ngOnInit(): void {
+    this.getDocumentos();
+  }
+  // Obtener los documentos
+  getDocumentos(): void {
+    // Obtener los Términos y Condiciones
+    this.regulatoriosService.getCurrentVersion('Términos y condiciones').subscribe(
+      (data) => {
+        console.log(data);
+        this.terminosCondiciones = data.contenido;
+        this.titulo = data.titulo;
+        this.versionDocumento = data.version;
+        this.fechaVigencia = data.fecha_vigencia;
+      },
+      (error) => {
+        console.error('Error al obtener el deslinde', error);
+      }
+    );
+  }
 }
+
