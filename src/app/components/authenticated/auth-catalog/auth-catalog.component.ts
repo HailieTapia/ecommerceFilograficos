db6b28d4a6adfa81a739f37d2dc7f248eaf92e13
HailieTapia <136946909+HailieTapia@users.noreply.github.com>
2025-03-20 05:34:40 -0600

    modified:   src/app/app.routes.ts 	new file:   src/app/components/authenticated/auth-catalog/auth-catalog.component.css 	new file:   src/app/components/authenticated/auth-catalog/auth-catalog.component.html 	new file:   src/app/components/authenticated/auth-catalog/auth-catalog.component.spec.ts 	new file:   src/app/components/authenticated/auth-catalog/auth-catalog.component.ts 	new file:   src/app/components/authenticated/auth-catalog/filter-sidebar/filter-sidebar.component.css 	new file:   src/app/components/authenticated/auth-catalog/filter-sidebar/filter-sidebar.component.html 	new file:   src/app/components/authenticated/auth-catalog/filter-sidebar/filter-sidebar.component.spec.ts 	new file:   src/app/components/authenticated/auth-catalog/filter-sidebar/filter-sidebar.component.ts 	new file:   src/app/components/authenticated/auth-catalog/product-detail-a/product-detail-a.component.css 	new file:   src/app/components/authenticated/auth-catalog/product-detail-a/product-detail-a.component.html 	new file:   src/app/components/authenticated/auth-catalog/product-detail-a/product-detail-a.component.spec.ts 	new file:   src/app/components/authenticated/auth-catalog/product-detail-a/product-detail-a.component.ts

---
+++ src/app/components/authenticated/auth-catalog/auth-catalog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-catalog',
+  standalone: true,
+  imports: [],
+  templateUrl: './auth-catalog.component.html',
+  styleUrl: './auth-catalog.component.css'
+})
+export class AuthCatalogComponent {
+
+}
---
+++ src/app/components/authenticated/auth-catalog/product-detail-a/product-detail-a.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-detail-a',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-detail-a.component.html',
+  styleUrl: './product-detail-a.component.css'
+})
+export class ProductDetailAComponent {
+
+}
