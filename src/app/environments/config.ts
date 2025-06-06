f8c51515a6ceb5db2b1792c397ce4989d35ebd17
LUISGERARDO11 <luis34julieta@gmail.com>
2025-06-06 14:55:32 -0600

    backup casi listo

--- src/app/environments/config.ts
+++ src/app/environments/config.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    baseUrl: 'http://localhost:3000/api', 
+    baseUrl: 'https://backend-filograficos.vercel.app/api', 
     recaptchaSiteKey: '6Lck9V8qAAAAAEnjZRJxMKa27R1P_GWppjLuxmbG',
     vapidKey: 'BAYyMOMC8yptUWH2E3n9nugPQBpVPnBtF0CsR55SL818_hG3pVTrH1q6bTW7fWniLQaFN3_U2f2g1LHMgKHnLA4'
 };
