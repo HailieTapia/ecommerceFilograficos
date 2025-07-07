ca658a3110e4c6d003a65a65f2b6801ca7a1fa7f
Hailie <htapiahailie@gmail.com>
2025-07-07 15:32:55 -0600

    modified:   src/app/components/administrator/shared/direccion.json 	modified:   src/app/components/authenticated/profile/addresses/addresses.component.html 	modified:   src/app/components/authenticated/profile/addresses/addresses.component.ts 	modified:   src/app/components/authenticated/profile/profile.component.html 	modified:   src/app/components/authenticated/profile/profile.component.ts

--- src/app/components/authenticated/profile/addresses/addresses.component.ts
+++ src/app/components/authenticated/profile/addresses/addresses.component.ts
@@ -5,18 +5,21 @@ import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, Validators } 
 import { ToastService } from '../../../services/toastService';
 import { ModalComponent } from '../../../../modal/modal.component';
 import * as addressData from '../../../administrator/shared/direccion.json';
+
 @Component({
   selector: 'app-addresses',
   standalone: true,
   imports: [ModalComponent, CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './addresses.component.html',
-  styleUrl: './addresses.component.css'
+  styleUrls: ['./addresses.component.css']
 })
 export class AddressesComponent implements OnInit {
   @ViewChild('modal') modal!: ModalComponent;
   userProfile: any = {};
   addressForm: FormGroup;
-  address: any = addressData;
+  addressData: any[] = (addressData as any).default || addressData;
+  uniquePostalCodes: string[] = [];
+  filteredAsentamientos: string[] = [];
 
   constructor(
     private toastService: ToastService,
@@ -24,11 +27,15 @@ export class AddressesComponent implements OnInit {
     private fb: FormBuilder
   ) {
     this.addressForm = this.fb.group({
-      street: ['', [Validators.required, Validators.maxLength(100),Validators.minLength(3), Validators.pattern(/^[a-zA-Z0-9\sáéíóúÁÉÍÓÚñÑ.,-]+$/)]],
-      city: ['',Validators.required],
-      state: ['',Validators.required],
-      postal_code: ['',Validators.required],
+      street: ['', [Validators.required, Validators.maxLength(255), Validators.minLength(3), Validators.pattern(/^[a-zA-Z0-9\sáéíóúÁÉÍÓÚñÑ.,-]+$/)]],
+      asentamiento: ['', Validators.required],
+      postal_code: ['', Validators.required],
+      city: [{ value: 'Pachuca', disabled: true }],
+      state: [{ value: 'Hidalgo', disabled: true }]
     });
+    this.uniquePostalCodes = [...new Set(this.addressData
+      .filter(item => item.estado === 'Hidalgo' && item.municipio === 'Pachuca de Soto')
+      .map(item => item.cp.toString()))];
   }
 
   ngOnInit(): void {
@@ -42,31 +49,31 @@ export class AddressesComponent implements OnInit {
   }
 
   openCreateModal(): void {
-    this.addressForm.reset();
+    this.addressForm.reset({ city: 'Pachuca', state: 'Hidalgo' });
+    this.filteredAsentamientos = [];
     this.modal.open();
-  }
-
-  getUserInfo(): void {
-    this.userService.getProfile().subscribe({
-      next: (profile) => {
-        this.userProfile = profile;
-      },
-      error: (error) => {
-        this.toastService.showToast(error?.error?.message || 'Error al obtener el perfil', 'error');
-      },
-    });
   }
 
   openEditModal(): void {
     this.userService.getProfile().subscribe({
       next: (response) => {
-        if (response.Addresses && response.Addresses.length > 0) {
-          const address = response.Addresses[0]; // Usamos la primera dirección AJUSTES DE BACKEND SI SE NECESITARA LAS DIRECCIOENS
+        if (response.address) {
+          const address = response.address;
+          const streetParts = address.street.split(', ');
+          const street = streetParts[0] || '';
+          const asentamiento = streetParts[1] || ''; // Extrae el asentamiento
+          const postalCode = address.postal_code;
+
+          // Filtrar asentamientos antes de patchValue
+          this.filterAsentamientos(postalCode);
+          
+          // Rellenar el formulario
           this.addressForm.patchValue({
-            street: address.street,
-            city: address.city,
-            state: address.state,
-            postal_code: address.postal_code,
+            street,
+            asentamiento, // Asigna el asentamiento parseado
+            postal_code: postalCode,
+            city: 'Pachuca',
+            state: 'Hidalgo'
           });
           this.modal.open();
         } else {
@@ -76,21 +83,47 @@ export class AddressesComponent implements OnInit {
       error: (err) => {
         console.error('Error al obtener la dirección:', err);
         this.toastService.showToast('Error al cargar la dirección', 'error');
-      },
+      }
     });
   }
+
+  filterAsentamientos(postalCode?: string): void {
+    if (postalCode) {
+      this.filteredAsentamientos = this.addressData
+        .filter(item => item.estado === 'Hidalgo' && item.municipio === 'Pachuca de Soto' && item.cp.toString() === postalCode)
+        .map(item => item.asentamiento);
+    } else {
+      const selectedCp = this.addressForm.get('postal_code')?.value;
+      if (selectedCp) {
+        this.filteredAsentamientos = this.addressData
+          .filter(item => item.estado === 'Hidalgo' && item.municipio === 'Pachuca de Soto' && item.cp.toString() === selectedCp)
+          .map(item => item.asentamiento);
+      } else {
+        this.filteredAsentamientos = [];
+      }
+    }
+    this.addressForm.get('asentamiento')?.reset();
+  }
+
   savePerfil(): void {
     if (this.addressForm.invalid) {
       this.toastService.showToast('Formulario inválido. Revisa los campos.', 'error');
       return;
     }
 
-    const addressData = this.addressForm.value;
+    const formValue = this.addressForm.getRawValue();
+    const selectedAsentamiento = this.addressData.find(item => item.asentamiento === formValue.asentamiento && item.cp.toString() === formValue.postal_code);
+    const addressData = {
+      street: `${formValue.street}, ${formValue.asentamiento}, ${selectedAsentamiento?.zona || 'Urbano'}, ${selectedAsentamiento?.tipo || 'Colonia'}`,
+      city: 'Pachuca',
+      state: 'Hidalgo',
+      postal_code: formValue.postal_code,
+      is_primary: true
+    };
 
-    if (this.userProfile.Addresses && this.userProfile.Addresses.length > 0) {
+    if (this.userProfile.address) {
       this.userService.updateUserProfile(addressData).subscribe({
         next: () => {
-          console.log(addressData);
           this.toastService.showToast('Dirección actualizada exitosamente', 'success');
           this.getUserInfo();
           this.modal.close();
@@ -98,12 +131,11 @@ export class AddressesComponent implements OnInit {
         error: (err) => {
           const errorMessage = err?.error?.message || 'Error al actualizar la dirección';
           this.toastService.showToast(errorMessage, 'error');
-        },
+        }
       });
     } else {
       this.userService.addAddress(addressData).subscribe({
         next: () => {
-          console.log(addressData);
           this.toastService.showToast('Dirección creada exitosamente', 'success');
           this.getUserInfo();
           this.modal.close();
@@ -111,8 +143,19 @@ export class AddressesComponent implements OnInit {
         error: (err) => {
           const errorMessage = err?.error?.message || 'Error al crear la dirección';
           this.toastService.showToast(errorMessage, 'error');
-        },
+        }
       });
     }
+  }
+
+  getUserInfo(): void {
+    this.userService.getProfile().subscribe({
+      next: (profile) => {
+        this.userProfile = profile;
+      },
+      error: (error) => {
+        this.toastService.showToast(error?.error?.message || 'Error al obtener el perfil', 'error');
+      }
+    });
   }
 }
