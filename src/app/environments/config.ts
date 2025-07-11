12ad88b200b14d378a0786217a2ce083fcdf1f35
LUISGERARDO11 <luis34julieta@gmail.com>
2025-07-11 16:22:40 -0600

    cambio de clientId para implementacion real

--- src/app/environments/config.ts
+++ src/app/environments/config.ts
@@ -3,7 +3,7 @@ export const environment = {
   baseUrl: 'https://backend-filograficos.vercel.app/api',
   recaptchaSiteKey: '6Lck9V8qAAAAAEnjZRJxMKa27R1P_GWppjLuxmbG',
   vapidKey: 'BAYyMOMC8yptUWH2E3n9nugPQBpVPnBtF0CsR55SL818_hG3pVTrH1q6bTW7fWniLQaFN3_U2f2g1LHMgKHnLA4',
-  clientId: 'amzn1.application-oa2-client.b6c7598ea28b41efa0f7cfa3e306f67b',
+  clientId: 'amzn1.application-oa2-client.686611e35ce04198b886556bda1981b0',
   alexaRedirectUrls: [
     'https://layla.amazon.com/api/skill/link/M34IVTO0VOKV0U',
     'https://pitangui.amazon.com/api/skill/link/M34IVTO0VOKV0U',
