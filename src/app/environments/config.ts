60a0f254dce4531b227e2b8003d6b1f4defd0bed
LUISGERARDO11 <luis34julieta@gmail.com>
2025-07-11 14:05:22 -0600

    cambio en enviroment

--- src/app/environments/config.ts
+++ src/app/environments/config.ts
@@ -1,9 +1,9 @@
 export const environment = {
-  production: false,
+  production: true,
   baseUrl: 'https://backend-filograficos.vercel.app/api',
   recaptchaSiteKey: '6Lck9V8qAAAAAEnjZRJxMKa27R1P_GWppjLuxmbG',
   vapidKey: 'BAYyMOMC8yptUWH2E3n9nugPQBpVPnBtF0CsR55SL818_hG3pVTrH1q6bTW7fWniLQaFN3_U2f2g1LHMgKHnLA4',
-  clientId: 'amzn1.application-oa2-client.b6c7598ea28b41efa0f7cfa3e306f67b',
+  clientId: 'filograficos-alexa-client-id-2025',
   alexaRedirectUrls: [
     'https://layla.amazon.com/api/skill/link/M34IVTO0VOKV0U',
     'https://pitangui.amazon.com/api/skill/link/M34IVTO0VOKV0U',
