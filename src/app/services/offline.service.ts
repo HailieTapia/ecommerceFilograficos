63b0ec3019a173033c200993442c75831aef8427
LUISGERARDO11 <luis34julieta@gmail.com>
2025-10-25 17:59:35 -0600

    configuracion inicial para capacidades pwa (instalable) asi como manejo offline basico

---
+++ src/app/services/offline.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, fromEvent } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OfflineService {
+  private onlineStatus = new BehaviorSubject<boolean>(navigator.onLine);
+  public status$ = this.onlineStatus.asObservable(); // Añadir 'public' explícitamente
+
+  constructor() {
+    fromEvent(window, 'online').subscribe(() => this.onlineStatus.next(true));
+    fromEvent(window, 'offline').subscribe(() => this.onlineStatus.next(false));
+  }
+}
